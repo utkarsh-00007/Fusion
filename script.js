92a332ef0ad3a149fd307833d334758624f8f6a2
Utkarsh Dwivedi <89937145+utkarsh-00007@users.noreply.github.com>
2023-03-18 23:56:49 +0530

    Updated all

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ let songs = [
     {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
     {songName: "Sakhiyaan - Maninder Buttar", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Malang Sajna - Sachet Parampara", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Akhar - Amrinder Gill", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
     {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
 
